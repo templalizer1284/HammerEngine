0e5dc77bac4039a58acd05e8370501bb0374583f
aca <aca@acas-MacBook-Pro.local>
2025-02-19 12:38:27 +0100

    Rock solid foundation, development begins

--- hammer.h
+++ hammer.h
@@ -17,6 +17,24 @@
 #include <raylib.h>
 #include <raymath.h>
 
+// macros
+#define HAMMERCFG "hammercfg"
+#define TITLE "Hammer Engine"
+#define MAX_MODELS 1024
+
+#define SEP "/"
+
+#if defined(_WIN32) || defined(_WIN64)
+#undef SEP
+#define SEP "\\"
+#endif
+
+#ifdef DEBUG
+	#define LOG(fmt, ...) printf(fmt __VA_OPT__(,) __VA_ARGS__)
+#else
+	#define LOG(...) ((void)0)
+#endif
+
 // i will not write this crap everytime
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -27,7 +45,8 @@ typedef struct h_Window h_Window;
 typedef struct h_Model h_Model;
 typedef struct h_Entity h_Entity;
 typedef struct h_Config h_Config;
-nn
+typedef struct h_EngineState h_EngineState;
+
 // fundecl
 HE_DECL u8 h_HammerRun(void);
 HE_DECL u8 h_WindowInit(void);
@@ -64,35 +83,33 @@ struct h_Config {
 	const char *level_config;
 };
 
+struct h_EngineState {
+	h_Window window;
+	Camera camera;
+	h_Model models[MAX_MODELS];
+	u16 model_count;
+	u8 fps;
+};
+
 // globals
-#define SEP "/"
+static h_EngineState engine = { .window.title = TITLE,
+				.camera = {0},
+				.models = {0},
+				.model_count = 0,
+				.fps = 30 };
 
-#ifdef _WIN32
-#define SEP "\\"
-#endif
-
-#ifdef _WIN64
-#define SEP "\\"
-#endif
-
-#define HAMMERCFG "hammercfg"
-static h_Window window = { 0 };
-static Camera camera = { 0 };
-static h_Model models[65536];
-
-static u16 models_count = 0;
-
+// fundef
 u8 h_HammerRun(void) {
 
-	printf("Hammer Engine Running, BattleCruiser operational.\n");
+	LOG("Hammer Engine Running, BattleCruiser operational.\n");
 
 	if(h_WindowInit()) {
-		perror("Window Initialization failed. Aborting.\n");
+		perror("Window Initialization failed. Aborting.");
 		return 1;
 	}
 	
 	if(h_Loop()) {
-		perror("Main loop error. Aborting.\n");
+		perror("Main loop error. Aborting.");
 		return 1;
 	}
 
@@ -104,20 +121,26 @@ u8 h_WindowInit(void) {
 		FILE *fp = fopen(HAMMERCFG, "r");
 		char tmp[64];
 
+		if(fp == NULL) {
+			LOG("HAMMERCFG failed to open, using default values.\n");
+			engine.window.width = 800;
+			engine.window.height = 600;
+		}
+
 		#define ff fscanf(fp, "%63s", tmp)
-		while(ff != EOF) {
+		while(ff == 1) {
 			if(strcmp(tmp, "width") == 0) {
 				ff;
-				window.width = atoi(tmp);
+				engine.window.width = atoi(tmp);
 			}
 
 			else if(strcmp(tmp, "height") == 0) {
 				ff;
-				window.height = atoi(tmp);
+				engine.window.height = atoi(tmp);
 			}
 
 			else {
-				perror("Syntax error in HAMMERCFG.\n");
+				perror("Syntax error in HAMMERCFG.");
 				return 1;
 			}
 		}
@@ -126,7 +149,9 @@ u8 h_WindowInit(void) {
 	}
 
 	else {
-		perror("HAMMERCFG failed to open.\n");
+		LOG("HAMMERCFG failed to open, using default settings.\n");
+		engine.window.width = 800;
+		engine.window.height = 600;
 		return 1;
 	}
 
@@ -134,14 +159,16 @@ u8 h_WindowInit(void) {
 	SetTraceLogLevel(LOG_WARNING);
 
 	// Pop-up the window
-	InitWindow(window.width, window.height, "Hammer Engine");
+	InitWindow(engine.window.width, engine.window.height, engine.window.title);
+
+	SetTargetFPS(engine.fps);
 
 	// Setting up camera SH-like style.
-	camera.position = (Vector3){ 0.0f, 5.0f, -7.0f };
-	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
-	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-	camera.fovy = 45.0f;
-	camera.projection = CAMERA_PERSPECTIVE;
+	engine.camera.position = (Vector3){ 0.0f, 5.0f, -7.0f };
+	engine.camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+	engine.camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+	engine.camera.fovy = 45.0f;
+	engine.camera.projection = CAMERA_PERSPECTIVE;
 
 	return 0;
 }
@@ -150,7 +177,7 @@ u8 h_Loop(void) {
 
 	while(!WindowShouldClose()) {
 		BeginDrawing();
-			BeginMode3D(camera);
+			BeginMode3D(engine.camera);
 
 		ClearBackground(DARKBLUE);
 		DrawFPS(10,10);
